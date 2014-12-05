3acf2792bdc7f460b77aefa11df5c7fe0e65d683
Matt Self <self.matt@gmail.com>
2014-12-05 14:05:50 -0700

    fun

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strings"
 	"time"
+	"unicode"
 	"unicode/utf8"
 )
 
@@ -43,23 +44,24 @@ type item struct {
 //  useExtension(name, left,right, function replaceTokens(input string) output string)
 func main() {
 	var buffer bytes.Buffer
-	fmt.Println("hello")
-	l := lex("test", "blah adfasdf lba **hasdf** alb asdfh [ab]lasdf\n blah asdfasdf **asdf**")
+	testValue := "This is the start of a sentence. [[link]] \n== Now a heading w/ **not parsed bold**== some words **some bold words** a sentence on two lines\n the other line //italics//"
+	l := lex("test", testValue)
 	fmt.Println(l)
 	for {
 
 		i := l.nextItem()
-		fmt.Println("GOT ONE ----v")
 		fmt.Println(i.val)
 		buffer.WriteString(i.val)
-		fmt.Println("GOT ONE ----^")
 		if l.state == nil {
 			break
 		}
 		//		fmt.Println(item)
 	}
-	fmt.Println("done")
+	fmt.Println("-------")
+	fmt.Println(testValue)
+	fmt.Println("-------")
 	fmt.Println(buffer.String())
+	fmt.Println("-------")
 	time.Sleep(5 * time.Second)
 	fmt.Println("You're boring; I'm leaving.")
 
@@ -70,8 +72,6 @@ func lex(name, input string) *lexer {
 		name:       name,
 		input:      input,
 		state:      lexText,
-		rightDelim: "**",
-		leftDelim:  "**",
 		items:      make(chan item, 2),
 		delimiters: make(map[string]*Delimiter),
 	}
@@ -82,25 +82,24 @@ func lex(name, input string) *lexer {
 
 type Delimiter struct {
 	name  string
-	left  string
-	right string
+	delim string
+	lexFn stateFn
 }
 
-func (l *lexer) addDelimiter(name string, left string, right string) {
-	delim, ok := l.delimiters[name]
+func (l *lexer) addDelimiter(name string, delim string, lexFn stateFn) {
+	_, ok := l.delimiters[name]
 	if !ok {
 		fmt.Println("A delimiter of that name already exists", delim)
 		return
 	}
 	//	var delimiter = Delimiter{left: left, right: right}
-	l.delimiters[name] = &Delimiter{left: left, right: right}
+	l.delimiters[name] = &Delimiter{name: name, delim: delim, lexFn: lexFn}
 
 }
 func (l *lexer) nextItem() item {
 	for {
 		select {
 		case item := <-l.items:
-			fmt.Println("something was lex-ed, let's return it to the caller")
 			return item
 		default:
 			//	fmt.Println(l.state)
@@ -119,16 +118,37 @@ func (l *lexer) nextItem() item {
 
 func lexText(l *lexer) stateFn {
 	for {
-		fmt.Println(l.input[l.pos:])
-		if strings.HasPrefix(l.input[l.pos:], l.leftDelim) {
-			fmt.Println("leftMeta hit")
-			if l.pos > l.start {
-				fmt.Println("l.pos > l.start", l.pos, l.start)
-				l.emit(1)
-			}
+		//fmt.Println(l.input[l.pos:])
 
-			fmt.Println("leftMeta hit")
-			return lexLeftDelim
+		if strings.HasPrefix(l.input[l.pos:], "**") {
+			if l.pos > l.start {
+				l.emit(77)
+			}
+			return lexEmphasis
+		}
+		if strings.HasPrefix(l.input[l.pos:], "//") {
+			if l.pos > l.start {
+				l.emit(77)
+			}
+			return lexItalics
+		}
+		if strings.HasPrefix(l.input[l.pos:], "\n") {
+			if l.pos > l.start {
+				l.emit(77)
+			}
+			return lexNewLine
+		}
+		if strings.HasPrefix(l.input[l.pos:], "=") {
+			if l.pos > l.start {
+				l.emit(77)
+			}
+			return lexHeading
+		}
+		if strings.HasPrefix(l.input[l.pos:], "[[") {
+			if l.pos > l.start {
+				l.emit(77)
+			}
+			return lexLink
 		}
 		if l.next() == eof {
 			break
@@ -157,6 +177,101 @@ func (l *lexer) errorf(format string, args ...interface{}) stateFn {
 	return nil
 }
 
+func lexNewLine(l *lexer) stateFn {
+	fmt.Println("lexingNewLine")
+	l.pos += len("\n")
+	//	if strings.HasPrefix(l.input[l.pos:], leftComment) {
+	//		return lexComment
+	//	}
+	l.emit(89) //TODO: reintroduce if needed
+	//l.parenDepth = 0
+	return lexText
+}
+
+func lexItalics(l *lexer) stateFn {
+	fmt.Println("lexingItalics")
+	l.pos += len("//")
+	//	if strings.HasPrefix(l.input[l.pos:], leftComment) {
+	//		return lexComment
+	//	}
+	l.emit(89) //TODO: reintroduce if needed
+	//l.parenDepth = 0
+	return lexText
+}
+func lexLink(l *lexer) stateFn {
+	fmt.Println("lexingLink")
+
+	l.pos += len("[[")
+
+	rightLink := "]]"
+	i := strings.Index(l.input[l.pos:], rightLink)
+	if i < 0 {
+		return l.errorf("unclosed link")
+	}
+	l.pos += len(rightLink) + i
+	l.emit(88)
+	return lexText
+
+}
+
+// ignore skips over the pending input before this point.
+func (l *lexer) ignore() {
+	l.start = l.pos
+}
+func lexHeading(l *lexer) stateFn {
+	fmt.Println("lexingHeading")
+	//l.next() //get past the line break
+	//TODO: THIS MUST BE ON A NEW LINE, and must have a space after the initial heading ==
+	headingCount := 0
+	fmt.Println("current", l.input[l.pos:l.pos+4])
+	fmt.Println("1", string(l.peek()))
+
+	for isHeading(l.peek()) {
+		fmt.Println("heading -yes")
+		headingCount++
+		l.next()
+	}
+
+	fmt.Println("2", string(l.peek()))
+	if !isSpace(l.peek()) {
+		fmt.Println("no space, not a heading")
+		l.next()
+		return lexText
+	} else {
+
+		fmt.Println("hooray . . . . . . . .. . . .")
+	}
+	rightHeading := makeHeading(headingCount)
+	fmt.Println("headingCount:", headingCount)
+	fmt.Println("rightHeading:", rightHeading)
+	i := strings.Index(l.input[l.pos:], rightHeading)
+	if i < 0 {
+
+		//an unclosed heading is optional in creole. so we should go until new line \n?
+		return l.errorf("unclosed heading")
+	}
+	fmt.Println("head + i", headingCount+i)
+	l.pos += headingCount + i
+	//l.pos += headingCount
+	l.emit(88)
+	return lexText
+}
+func makeHeading(n int) string {
+	var buffer bytes.Buffer
+	for i := 0; i < n; i++ {
+		buffer.WriteString("=")
+	}
+	return buffer.String()
+}
+func isHeading(r rune) bool {
+	return r == '='
+}
+func lexEmphasis(l *lexer) stateFn {
+	fmt.Println("lexingEmphasis")
+	l.pos += len("**")
+	l.emit(99) //TODO: reintroduce if needed
+	return lexText
+}
 func lexLeftDelim(l *lexer) stateFn {
 	fmt.Println("lexLeft")
 	l.pos += len(l.leftDelim)
@@ -204,4 +319,29 @@ func (l *lexer) run() {
 func (l *lexer) emit(t itemType) {
 	l.items <- item{t, l.start, l.input[l.start:l.pos]}
 	l.start = l.pos
+}
+
+// peek returns but does not consume the next rune in the input.
+func (l *lexer) peek() rune {
+	r := l.next()
+	l.backup()
+	return r
+}
+
+// backup steps back one rune. Can only be called once per call of next.
+func (l *lexer) backup() {
+	l.pos -= l.width
+}
+
+// isSpace reports whether r is a space character.
+func isSpace(r rune) bool {
+	//fmt.Println("isSpace", string(r))
+	//fmt.Println("rune", r)
+	return string(r) == " " || string(r) == "\t"
+	//return unicode.IsSpace(r)
+}
+
+// isAlphaNumeric reports whether r is an alphabetic, digit, or underscore.
+func isAlphaNumeric(r rune) bool {
+	return r == '_' || unicode.IsLetter(r) || unicode.IsDigit(r)
 }
